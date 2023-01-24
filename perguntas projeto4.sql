4eaaf35c9e5fa3b6603ed90c19b4a17f9afddbf4
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-01-24 19:53:25 -0300

    Add files via upload

---
+++ perguntas projeto4.sql
@@ -0,0 +1,47 @@
+SELECT * FROM projeto4.houses_v1;
+
+select
+	house_name, region
+from 
+	houses_v1
+where 
+	region 
+like
+	'north%';
+    
+    
+    
+select
+    season, episode, title, rating
+from
+	got_episodes_v4
+where
+	rating > 9.5 
+order by
+	rating
+desc;
+
+
+select
+	actor, Episodes_appeared 
+from
+	characters_v4
+order by
+	episodes_appeared desc
+limit 10;
+
+
+select
+	actor, characters 
+from
+	characters_v4
+where
+	characters
+like 
+	'%stark';
+    
+    
+select distinct
+	director
+from
+	got_episodes_v4;
